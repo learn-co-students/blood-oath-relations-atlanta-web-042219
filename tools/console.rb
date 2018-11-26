4e3ad862403a7032df84295515d178c19b363cd4
Michael Cheng <michael.cheng@flatironschool.com>
2018-11-26 12:03:41 -0500

    join cult

---
+++ tools/console.rb
@@ -0,0 +1,12 @@
+require_relative '../config/environment.rb'
+
+def reload
+  load 'config/environment.rb'
+end
+# Insert code here to run before hitting the binding.pry
+# This is a convenient place to define variables and/or set up new object instances,
+# so they will be available to test and play around with in your console
+
+binding.pry
+
+puts "Mwahahaha!" # just in case pry is buggy and exits
